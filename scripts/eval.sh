c988d91faa9ea1f139a269e78ee5e96e0c993698
Adrian Cheung <adrian.p.cheung@gmail.com>
2024-11-03 18:24:37 -0500

    change model path

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -3,7 +3,8 @@ split=forget01_perturbed
 model=phi
 lr=2e-5
 model_family=phi
-model_path=./data/locus/llm_weights/zhilif/TOFU/ft_epoch5_lr2e-05_phi_full_wd0.01
+# model_path=./data/locus/llm_weights/zhilif/TOFU/ft_epoch5_lr2e-05_phi_full_wd0.01
+model_path=./data/locus/llm_weights/zhilif/TOFU/ft_epoch5_lr2e-05_phi_full_wd0.01/grad_ascent_2e-05_forget01_5
 
 CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=$master_port evaluate_util.py\
  model_family=$model_family split=$split\
